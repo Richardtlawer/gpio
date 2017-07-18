93752a00306782bca01117a784e8a8f5e67f5354
Richard Teye Lawer <richardlawer@gmail.com>
2017-07-18 14:53:15 +0000

    ifttt

---
+++ butoshutdown.py
@@ -0,0 +1,15 @@
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+
+led = LED(17)
+button = Button(2)
+
+def shutdwon():
+    shutdown
+
+
+
+
+button.when_pressed=shutdwon
+            
