d75b9e1c8e653ae72bf5855968f845fc23ee2a1e
Richard Teye Lawer <richardlawer@gmail.com>
2017-07-24 12:06:00 +0000

    project one

---
+++ projectwork_ifttt.py
@@ -0,0 +1,22 @@
+from gpiozero import LED, Button
+from time import sleep
+from signal import pause
+import urllib2
+
+led=LED(17)
+front_door_pressed=Button(2)
+
+def ifttt():
+      print "done" 
+      #led.on()
+      #sleep(2)
+      #led.off
+      urllib2.urlopen("https://maker.ifttt.com/trigger/front_door_pressed/with/key/hBgwVN2raqAmjrPLQIULaTWbAejD8fYJipCyiKL3jlC").read
+front_door_pressed.when_pressed==ifttt
+
+		#led.on()
+      		#sleep(2)
+      		#led.off()
+#ifttt() 
+pause()
+
